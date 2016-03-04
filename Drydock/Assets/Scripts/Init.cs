969984a9404b7636b4717711f7fca2a78ddb5a1a
Hadzabadza <hadzabadza@yandex.ru>
2016-03-04 11:26:37 +0000

    Initial commit

---
+++ Drydock/Assets/Scripts/Init.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Init : MonoBehaviour {
+	public Transform CardBack;
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 4; j++) {
+				Instantiate (CardBack, new Vector3 (i * 3.0F - 3.0F, j*4.5F-4.5F, 0), Quaternion.identity);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
