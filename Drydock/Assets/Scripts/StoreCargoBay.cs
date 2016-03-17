ebaaf86971dd573eca7a5896250f6ecd89e517f2
avano001 <andrea.vanoli92@gmail.com>
2016-03-17 15:32:45 +0000

    123
    123

---
+++ Drydock/Assets/Scripts/StoreCargoBay.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StoreCargoBay : MonoBehaviour
+{
+
+    private Text cardText;
+
+    void Start()
+    {
+        gameObject.GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
+        cardText = GameObject.Find("Description").GetComponent<Text>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnMouseEnter()
+    {
+        gameObject.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+        cardText.text = "CargoBays allows your ship to carry more resources to the next level.";
+    }
+
+    void OnMouseExit()
+    {
+        gameObject.GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
+        cardText.text = "";
+    }
+
+}
